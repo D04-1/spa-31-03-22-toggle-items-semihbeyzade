fd2307fa17ecc764ac855b9eb0dbde94708b44b9
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-03-31 15:57:10 +0200

    Changed js-datei

--- index.js
+++ index.js
@@ -3,35 +3,16 @@ const listGroupElem = document.querySelector('.list-group')
 const messageElem = document.querySelector('.message')
 
 
-btnToggleElem.addEventListener('click', ()=>{
+btnToggleElem.addEventListener('click', () => {
     listGroupElem.classList.toggle('d-none')
     btnToggleElem.innerHTML = 'Hide destinations'
 
 })
 
-listGroupElem.addEventListener('click', (e)=>{
-     console.log(e.target);
-    if(e.target.id === "rome"){
-        messageElem.innerText = ' You selected Rome'
-        messageElem.style.backgroundColor = '#FDD7AA'
+listGroupElem.addEventListener('click', (e) => {
 
-    }
-    else if(e.target.id === "athens"){
-        messageElem.innerText = ' You selected Athens'
-        messageElem.style.backgroundColor = '#FDD7AA'
-    }
-    else if(e.target.id === "bangkok"){
-        messageElem.innerText = ' You selected Bangkok'
-        messageElem.style.backgroundColor = '#FDD7AA'
-    }
-    else if(e.target.id === "amsterdam"){
-        messageElem.innerText = ' You selected Amsterdam'
-        messageElem.style.backgroundColor = '#FDD7AA'
-    }
-    else if(e.target.id === "istanbul"){
-        messageElem.innerText = ' You selected Istanbul'
-        messageElem.style.backgroundColor = '#FDD7AA'
-    }
+    messageElem.innerText = `You selected ${e.target.innerText}`
+    messageElem.style.backgroundColor = '#FDD7AA'
+
+
 })
-
-
